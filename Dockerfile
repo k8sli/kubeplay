95fd0de39e46fb368bfe5ab16f0aa045e382b7c4
Muzi Li <muzi502.li@gmail.com>
2021-08-21 16:57:34 +0800

    feat(*): add compose config files and install scripts (#5)
    * cleanup(build): remove os-tools build workflow in kubeplay
    
    * feat(*): add config file and install scripts

--- Dockerfile
+++ Dockerfile
@@ -2,24 +2,33 @@ FROM alpine:latest as downloader
 ARG SKOPEO_VERSION=v1.4.0
 ARG YQ_VERSION=v4.11.2
 ARG NERDCTL_VERSION=0.11.0
-ARG NGINX_VERSION=1.19
+ARG NGINX_VERSION=1.20-alpine
 ARG RERGISRRY_VERSION=2.7.1
-RUN apk --no-cache add wget ca-certificates
+ARG KUBESPRAY_VERSION=latest
+ARG KUBESPRAY_IMAGE=ghcr.io/k8sli/kubespray
+
 WORKDIR /tools
 RUN ARCH=$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/') \
+    && apk --no-cache add wget ca-certificates \
     && wget -q -k https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_${ARCH}  -O /tools/yq-linux-${ARCH} \
-    && wget -q -k https://github.com/containerd/nerdctl/releases/download/v${NERDCTL_VERSION}/nerdctl-full-${NERDCTL_VERSION}-linux-${ARCH}.tar.gz \
     && wget -q -k https://github.com/k8sli/skopeo/releases/download/v1.4.0/skopeo-linux-${ARCH} -O /tools/skopeo-linux-${ARCH} \
+    && wget -q -k https://github.com/containerd/nerdctl/releases/download/v${NERDCTL_VERSION}/nerdctl-full-${NERDCTL_VERSION}-linux-${ARCH}.tar.gz \
     && chmod a+x /tools/* \
     && ln -s /tools/skopeo-linux-${ARCH} /usr/bin/skopeo
 
-WORKDIR /images/compose
+WORKDIR /images
 RUN ARCH=$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/') \
     && skopeo copy --insecure-policy --src-tls-verify=false --override-arch ${ARCH} --additional-tag nginx:${NGINX_VERSION} \
        docker://docker.io/library/nginx:${NGINX_VERSION} docker-archive:nginx-${NGINX_VERSION}.tar \
     && skopeo copy --insecure-policy --src-tls-verify=false --override-arch ${ARCH} --additional-tag registry:${RERGISRRY_VERSION} \
-       docker://docker.io/library/registry:${RERGISRRY_VERSION} docker-archive:registry-${RERGISRRY_VERSION}.tar
+       docker://docker.io/library/registry:${RERGISRRY_VERSION} docker-archive:registry-${RERGISRRY_VERSION}.tar \
+    && skopeo copy --insecure-policy --src-tls-verify=false --override-arch ${ARCH} --additional-tag kubespray:${KUBESPRAY_VERSION} \
+       docker://${KUBESPRAY_IMAGE}:${KUBESPRAY_VERSION} docker-archive:kubespray-${KUBESPRAY_VERSION}.tar
 
 FROM scratch
+COPY . .
 COPY --from=downloader /tools /resources/nginx/tools
-COPY --from=downloader /images/compose /resources/images/compose
+COPY --from=downloader /images /resources/images
+# COPY --from=${OS_PACKAGES_IMAGE}:${OS_PACKAGE_REPO_TAG} / /resources/nginx
+# COPY --from=${KUBESPRAY_FILES_IMAGE}:${KUBESPRAY_REPO_TAG} / /resources/nginx
+# COPY --from=${KUBESPRAY_IMAGES_IMAGE}:${KUBESPRAY_REPO_TAG} / /resources/registry
---
+++ install.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash
+INSTALL_TYPE=$1
+: ${INSTALL_TYPE:=all}
+
+# Common utilities, variables and checks for all build scripts.
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Gather variables about bootstrap system
+USR_BIN_PATH=/usr/local/bin
+export PATH="${PATH}:${USR_BIN_PATH}"
+ARCH=$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')
+
+# Define glob vars
+KUBE_ROOT="$(cd "$(dirname "$0")" && pwd)"
+CERTS_DIR="${KUBE_ROOT}/config/certs"
+CONFIG_FILE="${KUBE_ROOT}/config.yaml"
+COMPOSE_YAML_FILE="${KUBE_ROOT}/compose.yaml"
+IMAGES_DIR="${KUBE_ROOT}/resources/images"
+COMPOSE_CONFIG_DIR="${KUBE_ROOT}/config/compose"
+INSTALL_STEPS_FILE="${KUBE_ROOT}/.install_steps"
+OUTPUT_ENV_FILE="${KUBE_ROOT}/.install-env.sh"
+RESOURCES_NGINX_DIR="${KUBE_ROOT}/resources/nginx"
+KUBESPRAY_CONFIG_DIR="${KUBE_ROOT}/config/kubespray"
+CA_CONFIGFILE="${KUBE_ROOT}/config/rootCA.cnf"
+
+# Import all functions from scripts/*.sh
+for file in ${KUBE_ROOT}/scripts/*.sh; do source ${file}; done
+
+# Get os release info
+if ! source /etc/os-release; then
+  errorlog "Every system that we officially support has /etc/os-release"
+  exit 1
+fi
+
+if [ ! -f ${CONFIG_FILE} ]; then
+  errorlog "The ${CONFIG_FILE} file is not existing"
+  exit 1
+fi
+
+usage(){
+  cat <<EOF
+Usage: install.sh [TYPE] [NODE_NAME]
+  The script is used for install kubernetes cluster
+
+Parameter:
+  [TYPE]\t  this param is used to determine what to do with the kubernetes cluster.
+  Available type as follow:
+    all              deploy compose addon and kubernetes cluster
+    compose          deploy nginx and registry server
+    deploy-cluster   install kubernetes cluster
+    remove-cluster   remove kubernetes cluster
+    add-node         add worker node to kubernetes cluster
+    remove-node      remove worker node to kubernetes cluster
+    debug            run debug mode for install or troubleshooting
+
+  [NODE_NAME] this param to choose node for kubespray to exceute.
+              Note: when [TYPE] is specified [add-node] or [remove-node] this parameter must be set
+              multiple nodes are separated by commas, example: node01,node02,node03
+
+EOF
+  exit 0
+}
+
+deploy_cluster(){
+  common::run_kubespray "bash /kubespray/run.sh deploy-cluster"
+}
+
+remove_cluster(){
+  common::run_kubespray "bash /kubespray/run.sh remove-cluster"
+}
+
+add_nodes(){
+  common::run_kubespray "bash /kubespray/run.sh add-node $1"
+}
+
+remove_nodes(){
+  common::run_kubespray "bash /kubespray/run.sh remove-node $1"
+}
+
+kubespray_debug(){
+  common::run_kubespray "/usr/bin/bash"
+}
+
+install_all(){
+  bootstrap
+  deploy_cluster
+}
+
+main(){
+  case ${INSTALL_TYPE} in
+    all)
+      install_all
+      ;;
+    compose)
+      bootstrap
+      ;;
+    cluster)
+      deploy_cluster
+      ;;
+    remove)
+      remove_cluster
+      remove_compose
+      ;;
+    remove-cluster)
+      remove_cluster
+      ;;
+    add-nodes)
+      ;;
+    remove-node)
+      ;;
+    debug)
+      kubespray_debug
+      ;;
+    -h|--help|help)
+      usage
+      ;;
+    *)
+      echowarn "unknow [TYPE] parameter: ${INSTALL_TYPE}"
+      usage
+      ;;
+  esac
+}
+
+main "$@"
